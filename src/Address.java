3e50340b5e2d8dac944fa3fc1b39dc9ac8095ed1
mustafa <mustafaqasimi8656@gmail.com>
2024-12-23 20:43:30 +0330

    Class Address added

---
+++ src/Address.java
@@ -0,0 +1,37 @@
+public class Address {
+    private String zipCode;
+    private String country;
+    private String city;
+    public Address(String zipCode, String country, String city) {
+        this.zipCode = zipCode;
+        this.country = country;
+        this.city = city;
+    }
+    public String getZipCode() {
+        return zipCode;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "zipCode='" + zipCode + '\'' +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
